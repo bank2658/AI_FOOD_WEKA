e35df3eb912b0e70a71db6f375a4efaa1d88faa0
Sitthiporn_Stp.b <sitthiporn.t@ku.th>
2018-04-20 19:10:33 +0700

    Create Project

---
+++ FOOD_APP/FOOD_APP/HOME_APP.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace FOOD_APP
+{
+    partial class HOME_APP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HOME_APP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 509);
+            this.Name = "HOME_APP";
+            this.Text = "HOME_FOOD";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ FOOD_APP/FOOD_APP/HOME_APP.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FOOD_APP
+{
+    public partial class HOME_APP : Form
+    {
+        public HOME_APP()
+        {
+            InitializeComponent();
+        }
+    }
+}
